df53c670278cc8101051dfb3b900a5454c2b6589
Sushanth-Personal <sushanthp.calicut@gmail.com>
2024-11-07 12:25:48 +0530

    Update: updated vite configurations

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: process.env.PORT || 4173, // Bind to the Render-provided port, fallback to 4173
+    host: true, // Expose the app to external IPs
+  },
+  build: {
+    outDir: 'dist', // Ensure that build output goes into the 'dist' folder
+  },
+});
